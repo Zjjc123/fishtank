2ee077a5126aa85ecf39c05f01154047e937ab6e
Jason Zhang <zjjc123@hotmail.com>
2025-06-19 02:02:00 -0700

    init

---
+++ Fishtank/FishtankApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishtankApp.swift
+//  Fishtank
+//
+//  Created by Jiajun Zhang on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FishtankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FishtankTests/FishtankTests.swift
@@ -0,0 +1,17 @@
+//
+//  FishtankTests.swift
+//  FishtankTests
+//
+//  Created by Jiajun Zhang on 6/19/25.
+//
+
+import Testing
+@testable import Fishtank
+
+struct FishtankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
