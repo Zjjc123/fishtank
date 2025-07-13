fb452e037ed55703dea9b4c534b7afc70f334d8f
Jason Zhang <zjjc123@hotmail.com>
2025-07-13 01:06:35 -0400

    update fishes

--- Fishtank/Models/FishDatabase.swift
+++ Fishtank/Models/FishDatabase.swift
@@ -17,6 +17,16 @@ struct FishDatabase {
     Fish(name: "Perch", imageName: "Perch", rarity: .common, size: .small),
     Fish(name: "Sardine", imageName: "Sardine", rarity: .common, size: .tiny),
     Fish(name: "Mackerel", imageName: "Mackerel", rarity: .common, size: .small),
+    Fish(name: "Molly", imageName: "Molly", rarity: .common, size: .small),
+    Fish(name: "Platy", imageName: "Platy", rarity: .common, size: .small),
+    Fish(name: "Zebrafish", imageName: "Zebrafish", rarity: .common, size: .small),
+    Fish(name: "Killifish", imageName: "Killifish", rarity: .common, size: .small),
+    Fish(name: "Tilapia", imageName: "Tilapia", rarity: .common, size: .medium),
+    Fish(name: "Goby", imageName: "Goby", rarity: .common, size: .small),
+    Fish(name: "Smelt", imageName: "Smelt", rarity: .common, size: .tiny),
+    Fish(name: "Chub", imageName: "Chub", rarity: .common, size: .small),
+    Fish(name: "Dace", imageName: "Dace", rarity: .common, size: .small),
+    Fish(name: "Roach", imageName: "Roach", rarity: .common, size: .small),
 
     // Uncommon Fish
     Fish(name: "Cod", imageName: "Cod", rarity: .uncommon, size: .medium),
@@ -24,22 +34,46 @@ struct FishDatabase {
     Fish(name: "Salmon", imageName: "Salmon", rarity: .uncommon, size: .medium),
     Fish(name: "Guppy", imageName: "Guppy", rarity: .uncommon, size: .small),
     Fish(name: "Goldfish", imageName: "Goldfish", rarity: .uncommon, size: .small),
+    Fish(name: "Flying Fish", imageName: "Flying Fish", rarity: .uncommon, size: .small),
+    Fish(name: "Mudskipper", imageName: "Mudskipper", rarity: .uncommon, size: .small),
+    Fish(name: "Neon Tetra", imageName: "Neon Tetra", rarity: .uncommon, size: .small),
+    Fish(name: "Glass Catfish", imageName: "Glass Catfish", rarity: .uncommon, size: .small),
+    Fish(name: "Gourami", imageName: "Gourami", rarity: .uncommon, size: .small),
 
     // Rare Fish
     Fish(name: "Sturgeon", imageName: "Sturgeon", rarity: .rare, size: .large),
     Fish(name: "Tuna", imageName: "Tuna", rarity: .rare, size: .large),
     Fish(name: "Marlin", imageName: "Marlin", rarity: .rare, size: .large),
     Fish(name: "Barracuda", imageName: "Barracuda", rarity: .rare, size: .medium),
+    Fish(name: "Electric Eel", imageName: "Electric Eel", rarity: .rare, size: .large),
+    Fish(name: "Arctic Char", imageName: "Arctic Char", rarity: .rare, size: .medium),
+    Fish(name: "Haddock", imageName: "Haddock", rarity: .rare, size: .medium),
+    Fish(name: "Red Snapper", imageName: "Red Snapper", rarity: .rare, size: .medium),
+    Fish(name: "Rockfish", imageName: "Rockfish", rarity: .rare, size: .medium),
+    Fish(name: "Jellyfish", imageName: "Jellyfish", rarity: .rare, size: .medium),
 
     // Epic Fish
     Fish(name: "Shark", imageName: "Shark", rarity: .epic, size: .huge),
     Fish(name: "Dolphin", imageName: "Dolphin", rarity: .epic, size: .large),
+    Fish(name: "Giant Squid", imageName: "Giant Squid", rarity: .epic, size: .huge),
+    Fish(name: "Sawfish", imageName: "Sawfish", rarity: .epic, size: .large),
+    Fish(name: "Tarpon", imageName: "Tarpon", rarity: .epic, size: .large),
+    Fish(name: "Sea Turtle", imageName: "Sea Turtle", rarity: .epic, size: .large),
+    Fish(name: "Ocean Sunfish", imageName: "Ocean Sunfish", rarity: .epic, size: .huge),
 
     // Legendary Fish
     Fish(name: "Manta Ray", imageName: "Manta Ray", rarity: .legendary, size: .huge),
     Fish(name: "Blue Whale", imageName: "Blue Whale", rarity: .legendary, size: .giant),
     Fish(name: "Orca", imageName: "Orca", rarity: .legendary, size: .giant),
     Fish(name: "Narwhal", imageName: "Narwhal", rarity: .legendary, size: .large),
+    Fish(name: "Megalodon", imageName: "Megalodon", rarity: .legendary, size: .giant),
+    Fish(name: "Leviathan", imageName: "Leviathan", rarity: .legendary, size: .giant),
+    Fish(name: "Kraken", imageName: "Kraken", rarity: .legendary, size: .giant),
+    Fish(name: "Coelacanth", imageName: "Coelacanth", rarity: .legendary, size: .large),
+    Fish(name: "Loch Ness Monster", imageName: "Loch Ness Monster", rarity: .legendary, size: .giant),
+    Fish(name: "Giant Oarfish", imageName: "Giant Oarfish", rarity: .legendary, size: .huge),
+    Fish(name: "Sea Serpent", imageName: "Sea Serpent", rarity: .legendary, size: .giant),
+    Fish(name: "Beluga Whale", imageName: "Beluga Whale", rarity: .legendary, size: .large),
   ]
 
   static func fishByRarity(_ rarity: FishRarity) -> [Fish] {
